a8557aff6a7dda0a6991916ebdca6714b79de25f
SelMiV <selivankin2005@gmail.com>
2026-04-24 02:46:08 +0300

    сделал класс обработчик оператора

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         NumberSystemConverter converter = new NumberSystemConverter();
+        OperationExecutor executor = new OperationExecutor();
 
-        System.out.println("Введите число: ");
-        int input = Integer.parseInt(scanner.nextLine());
+//        System.out.print("Введите число в десятичной системе: ");
+//        int input = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println(converter.fromDec(input, NumberSystem.BIN));
+//        System.out.println(converter.fromDec(input, NumberSystem.OCT));
+//        System.out.println(converter.fromDec(input, NumberSystem.HEX).toUpperCase());
+//
+//        System.out.print("Введите число в восьмиричной системе: ");
+//        String input1 = scanner.nextLine();
+//        System.out.println(converter.toDec(input1, NumberSystem.HEX));
 
-        System.out.println(converter.fromDec(input, NumberSystem.BIN));
-        System.out.println(converter.fromDec(input, NumberSystem.OCT));
-        System.out.println(converter.fromDec(input, NumberSystem.HEX));
+        System.out.println(executor.execute(4, 2, '/'));
     }
 }
